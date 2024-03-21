619e72dcbd8fe031b14fb423354b01a4f79cca73
ferblaca <fernandobc@ext.inditex.com>
2024-03-21 14:29:35 +0100

    observation batch

--- src/main/java/com/example/stream/kafka/demoKafkaBatch/DemoKafkaBatchApplication.java
+++ src/main/java/com/example/stream/kafka/demoKafkaBatch/DemoKafkaBatchApplication.java
@@ -2,12 +2,12 @@ package com.example.stream.kafka.demoKafkaBatch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.listener.BatchListenerFailedException;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -15,7 +15,7 @@ import java.util.function.Consumer;
 @SpringBootApplication
 public class DemoKafkaBatchApplication {
 
-    private Logger LOG = LoggerFactory.getLogger(DemoKafkaBatchApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DemoKafkaBatchApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoKafkaBatchApplication.class, args);
@@ -24,22 +24,35 @@ public class DemoKafkaBatchApplication {
     @Bean
     public ApplicationRunner runnerBatchSend(StreamBridge bridge) {
         return args -> {
-            for (int i = 0; i < 10; i++) {
-                bridge.send("producerBatch-out-0", "test-" + i);
+            for (int i = 0; i < 100; i++) {
+                bridge.send("producer-batch-out-0", "test-" + i);
             }
         };
     }
 
-    @Bean
+//    @Bean
+//    public ApplicationRunner runnerSend(StreamBridge bridge) {
+//        return args -> {
+//            for (int i = 0; i < 500; i++) {
+//                bridge.send("producer-single-out-0", "test-" + i);
+//            }
+//        };
+//    }
+
+    @Bean("consumerBatch")
     public Consumer<List<String>> consumerBatch() {
-        return s -> {
-            s.stream().forEach(s1 -> {
-                LOG.info(s1);
-                if (s1.equals("test-5")) {
-                    throw new BatchListenerFailedException(s1, 5);
-                }
-            });
-        };
+        return s -> s.forEach(s1 -> {
+            LOG.info(s1);
+            LOG.info("++++++++ CONSUMER Batch size: {}", s.size());
+            LOG.info("++++++++ MDC Batch: {}", MDC.getCopyOfContextMap());
+        });
     }
 
+//    @Bean("consumerSingle")
+//    public Consumer<String> consumerSingle() {
+//        return s -> {
+//            LOG.info("------- CONSUMER: {}", s);
+//            LOG.info("------- MDC: {}", MDC.getCopyOfContextMap());
+//        };
+//    }
 }
